a7858de34bc4415b700404272d6083e2b2b7dc74
francheska-vicente <francheska_vicente@dlsu.edu.ph>
2021-04-26 18:29:20 +0800

    Update classController.js

--- controllers/classController.js
+++ controllers/classController.js
@@ -5,14 +5,20 @@ const classController = {
         var c = req.params.coursecode;
         var query = {coursecode: c};
 
-        db.findOne('classes', query, function (result) {
-            res.render('class', result);
+        db.findOne('Class', query, function (result) {
+            res.render('Class', result);
         });
     },
 
     getDiscussions: function (req, res) {
     	var c = req.params.coursecode;
-        var query = {coursecode: c};
+        var query = {
+        	coursecode: c
+        };
+
+        db.findMany ('Discussions', query, function (result) {
+            res.render('Discussions', result);
+        });
     }
 }
 
