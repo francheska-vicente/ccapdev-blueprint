9a09435c107f7557f02ab31036e4c2e03a6c9a93
francheska-vicente <francheska_vicente@dlsu.edu.ph>
2021-04-26 17:34:10 +0800

    Create ReqsModel.js

---
+++ models/ReqsModel.js
@@ -0,0 +1,35 @@
+var mongoose = require('mongoose');
+
+var ReqsSchema = new mongoose.Schema({
+    classID : {
+        type: String,
+        required: true
+    },
+
+    userID: {
+        type: String,
+        required: true
+    },
+
+    desc: {
+        type: String,
+        required: true
+    },
+
+    typeOfReq : {
+        type: String,
+        required: true
+    },
+
+    postDate : {
+        type: Date,
+        required: true
+    },
+
+    deadline : {
+        type: Date,
+        required: true
+    }
+});
+
+module.exports = mongoose.model('Requirements', ReqsSchema);
