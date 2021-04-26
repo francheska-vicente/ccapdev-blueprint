45d43f06c5325781a294ee6061f2a41c3cb49dc0
francheska-vicente <francheska_vicente@dlsu.edu.ph>
2021-04-26 23:40:15 +0800

    Update classController.js

--- controllers/classController.js
+++ controllers/classController.js
@@ -1,6 +1,7 @@
 const db = require('../models/db.js');
 const Discussion = require('../models/DiscModel.js');
 const Course = require ('../models/ClassModel.js');
+const Note = require ('../models/NotesModel.js');
 
 const classController = {
 	getClass: function (req, res) {
@@ -32,6 +33,26 @@ const classController = {
         });
 
         res.render('discussions', classInfo, results);
+    },
+
+    getNotes : function (req, res) {
+        var c = req.params.classID;
+        var query = {
+            classID: c
+        };
+
+        var classInfo;
+        var results;
+        
+        db.findOne (Course, query, null, function (classInfo) {
+            classInfo = classInfo;
+        });
+
+        db.findMany (Note, query, null, function (err, result) {
+            results = result;
+        });
+
+        res.render('notes', classInfo, results);
     }
 }
 
