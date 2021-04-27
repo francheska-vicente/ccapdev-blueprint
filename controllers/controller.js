e22360a23a46ceeaf16ddc8376c1e0135b1c8e2a
Sophia Vista <75743382+sophia-vista@users.noreply.github.com>
2021-04-27 15:23:07 +0800

    help (2)

--- controllers/controller.js
+++ controllers/controller.js
@@ -72,9 +72,26 @@ const controller = {
 
     getDashboard: function (req, res) {
         var classIDs = ['123'];
-        db.findMany(Course, {classID : {$in : classIDs}}, '', function (result) {
-            res.render('dashboard', result);
-        });
+        // db.findMany(Course, {classID : {$in : classIDs}}, '', function (result) {
+        //     res.render('dashboard', result);
+        // });
+
+        var class1 = {
+            classname : "rawr",
+            coursecode :  "CCAPDEV",
+            classID : "1234"
+        }
+
+        var class2 = {
+            classname : "rawr2",
+            coursecode :  "CCAPDEV2",
+            classID : "4321"
+        }
+
+        var result = []
+        result.push(class1)
+        result.push(class2)
+        res.render('dashboard', result);
     }
 }
 
