b34fc69615de0b4af9cabe25b862e8ff196f05e4
francheska-vicente <francheska_vicente@dlsu.edu.ph>
2021-04-28 15:17:09 +0800

    Update classController.js

--- controllers/classController.js
+++ controllers/classController.js
@@ -130,6 +130,18 @@ const classController = {
 
     editComment : function (req, res) {
 
+    }, 
+
+    editDiscussions : function (req, res) {
+        var d = req.params.discID;
+
+         db.findOne (Discussion, {discID : d}, null, function (discInfo) {
+            discInfo.content = req.body.main_discussion_text;
+
+            db.updateOne (Discussion, {discID : d}, discInfo, function (result) {
+                res.redirect ('/classes/:classID/discussions/:discID');
+            });
+         });
     }
 }
 
