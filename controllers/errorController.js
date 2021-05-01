ab551a1e307b81ef25f0cced790f504f66ea3c44
Sophia Vista <75743382+sophia-vista@users.noreply.github.com>
2021-05-01 23:33:32 +0800

    added error 403

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -6,6 +6,12 @@ const errorController = {
         var err = new Error('You are not logged in!');
         err.status = 401;
         next(err);
+    },
+
+    get403: function (req, res, next) {
+        var err = new Error('You are not allowed to access this!');
+        err.status = 403;
+        next(err);
     }
 }
 
