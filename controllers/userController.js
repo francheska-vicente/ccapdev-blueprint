b97b8c088869182d904efab3335f95b079b19e0d
sophia-vista <75743382+sophia-vista@users.noreply.github.com>
2021-05-16 16:29:08 +0800

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,20 +6,26 @@ const Course = require ('../models/ClassModel.js');
 const userController = {
 
     getUserProfile: function (req, res) {
-        db.findOne(User, {username: req.params.username}, '', function (result) {
-            if(result) {
-                if(result.username == req.session.username) res.redirect('/profile');
-                else res.render('userprofile', result);
+        db.findOne(User, {username: req.params.username}, '', function (user) {
+            if(user) {
+                if(user.bday) {
+                    var d = new Date(user.bday);
+                    var datestring = ("0" + (d.getMonth()+1)).slice(-2) + "-" + ("0" + d.getDate()).slice(-2) + "-" + d.getFullYear();
+                    user.bday = datestring;
+                }
+                
+                if(user.username == req.session.username) res.redirect('/profile');
+                else res.render('userprofile', user);
             }
             else res.redirect('/error/404');
         });
     },
 
     getUserSchedule: function (req, res) {
-        db.findOne(User, {username: req.params.username}, '', function (result) {
-            if(result) {
-                if(result.username == req.session.username) res.redirect('/schedule');
-                else res.render('userschedule', result);
+        db.findOne(User, {username: req.params.username}, '', function (user) {
+            if(user) {
+                if(user.username == req.session.username) res.redirect('/schedule');
+                else res.render('userschedule', user);
             }
             else res.redirect('/error/404');
         });
