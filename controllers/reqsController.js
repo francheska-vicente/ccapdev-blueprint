743e22b476f80f4371b4cee0e6eb62e5f51ee125
francheska-vicente <francheska_vicente@dlsu.edu.ph>
2021-05-16 22:27:53 +0800

    Update reqsController.js

--- controllers/reqsController.js
+++ controllers/reqsController.js
@@ -57,26 +57,25 @@ const reqsController = {
         });
 
         db.findOne (Course, {classID : c}, null, function (result) {
-            coursecode = result.coursecode;
+            var reqs = {
+                classID : c,
+                username : username,
+                fName : fName,
+                lName : lName,
+                reqID : id,
+                title : req.body.title,
+                desc: req.body.paragraph_text,
+                deadline: req.body.deadline,
+                typeOfReq: req.body.type, 
+                coursecode : result.coursecode
+            };
+
+            db.insertOne (Reqs, reqs, function (result1) {
+                res.redirect ('/classes/' + c + '/requirements');
+            });
         });
 
-        var reqs = {
-            classID : c,
-            username : username,
-            fName : fName,
-            lName : lName,
-            reqID : id,
-            title : req.body.title,
-            desc: req.body.paragraph_text,
-            deadline: req.body.deadline,
-            typeOfReq: req.body.type, 
-            coursecode : coursecode
-        };
-
-        db.insertOne (Reqs, reqs, function (result) {
-            
-            res.redirect ('/classes/' + c + '/requirements');
-        });
+        
     },
 
     editReqsPost : function (req, res) {
