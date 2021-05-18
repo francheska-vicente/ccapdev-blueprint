0031ec115433b166568384f2cd3f7d6d189ae3d3
francheska-vicente <francheska_vicente@dlsu.edu.ph>
2021-05-18 15:11:20 +0800

    validator

--- controllers/signupController.js
+++ controllers/signupController.js
@@ -1,3 +1,5 @@
+const { validationResult } = require('express-validator');
+
 const db = require('../models/db.js');
 const User = require('../models/UserModel.js');
 
@@ -22,25 +24,41 @@ const signupController = {
     },
     
     postSignUp: function (req, res) {
-        var user = {
-            fName: req.body.fName.split(' ').map(w => w[0].toUpperCase() + w.substr(1).toLowerCase()).join(' '),
-            lName: req.body.lName.split(' ').map(w => w[0].toUpperCase() + w.substr(1).toLowerCase()).join(' '),
-            email:  req.body.email,
-            username: req.body.username.toLowerCase (),
-            password: req.body.password,
-            uni:  req.body.school,
-            bday: '',
-            phone: '',
-            degree: '',
-            bio: '',
-            classes: []
-        };
-        
-        db.insertOne(User, user, function(flag) {
-            if(flag) {
-                res.redirect('/register-success?fName=' + user.fName +'&lName=' + user.lName);
-            }
-        });
+        var errors = validationResult(req);
+
+        console.log ("heheh " + errors);
+        if (!errors.isEmpty()) 
+        {
+            errors = errors.errors;
+            var details = {};
+            
+            for(i = 0; i < errors.length; i++)
+                details[errors[i].param + 'Error'] = errors[i].msg;
+            console.log ("Mali repa bro");
+            res.render('signup', details);
+        }
+        else 
+        {
+            var user = {
+                fName: req.body.fName.split(' ').map(w => w[0].toUpperCase() + w.substr(1).toLowerCase()).join(' '),
+                lName: req.body.lName.split(' ').map(w => w[0].toUpperCase() + w.substr(1).toLowerCase()).join(' '),
+                email:  req.body.email,
+                username: req.body.username.toLowerCase (),
+                password: req.body.password,
+                uni:  req.body.school,
+                bday: '',
+                phone: '',
+                degree: '',
+                bio: '',
+                classes: []
+            };
+            
+            db.insertOne(User, user, function(flag) {
+                if(flag) {
+                    res.redirect('/register-success?fName=' + user.fName +'&lName=' + user.lName);
+                }
+            });
+        }  
     }
 }
 
