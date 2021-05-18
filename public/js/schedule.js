d60892f70ef8fefa6538bfbd2ef60afeed9ac854
sophia-vista <75743382+sophia-vista@users.noreply.github.com>
2021-05-18 20:38:03 +0800

    Update schedule.js

--- public/js/schedule.js
+++ public/js/schedule.js
@@ -1,5 +1,11 @@
 $(document).ready(function () {
     var times = [
+        '00:00', '00:15', '00:30', '00:45',
+        '01:00', '01:15', '01:30', '01:45',
+        '02:00', '02:15', '02:30', '02:45',
+        '03:00', '03:15', '03:30', '03:45',
+        '04:00', '04:15', '04:30', '04:45',
+        '05:00', '05:15', '05:30', '05:45',
         '06:00', '06:15', '06:30', '06:45',
         '07:00', '07:15', '07:30', '07:45',
         '08:00', '08:15', '08:30', '08:45',
@@ -17,15 +23,13 @@ $(document).ready(function () {
     var days = ['Sunday', 'Monday', 'Tuesday', 
         'Wednesday','Thursday', 'Friday', 'Saturday'];
 
-    var numrows = 48;
+    var numrows = 72;
     var numcols = 7;
 
     function countRowSpan(start, end) {
         var timeStart = new Date("01/01/2021 " + start);
         var timeEnd = new Date("01/01/2021 " + end);
-
-        var difference = timeEnd - timeStart;
-        return difference / 60 / 1000 / 15;
+        return (timeEnd - timeStart) / 60 / 1000 / 15;
     }
 
     function checkIfInClass(currtime, currday, result) {
