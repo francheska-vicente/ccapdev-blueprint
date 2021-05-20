49d284415f774c52139ff9a205bf46c33ff32dd5
sophia-vista <75743382+sophia-vista@users.noreply.github.com>
2021-05-21 00:47:30 +0800

    Update profileController.js

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const db = require('../models/db.js');
 const User = require('../models/UserModel.js');
@@ -19,7 +20,6 @@ const profileController = {
                     var d = new Date(user.bday);
                     var datestring = ("0" + (d.getMonth()+1)).slice(-2) + "-" + ("0" + d.getDate()).slice(-2) + "-" + d.getFullYear();
                     user.bday = datestring;
-                    console.log(datestring)
                 }
 
                 var notif = null;
@@ -63,29 +63,37 @@ const profileController = {
             // gets user from db
             db.findOne(User, {username: req.session.username}, '', function (user) {
 
-                // creates updated user object
-                var update = user;
-                if (req.body.username != '') update.username = req.body.username;
-                if (req.body.contact_no != '') update.phone = req.body.contact_no;
-                if (req.body.bday != '') update.bday = req.body.bday;
-                if (req.body.degree != '') update.degree = req.body.degree;
-                if (req.body.bio != '') update.bio = req.body.bio;
-                if (req.body.n_password != '') update.password = req.body.n_password;
+                // hashes password
+                bcrypt.hash(req.body.n_password, saltRounds, function(err, hash) {
 
-                // checks if password matches user's password
-                bcrypt.compare(req.body.password, user.password, function(err, equal) {
-                    if(equal) {
+                    // checks if password matches user's password
+                    bcrypt.compare(req.body.password, user.password, function(err, equal) {
+                        if(equal) {
 
-                        // updates user in db
-                        db.updateOne(User, {username: user.username}, update, function(flag) {
-                            if(flag) res.redirect('/profile?editsuccess=true');
-                            else res.redirect('/profile?editsuccess=false');
-                        });
-                    }
-                    else {
-                        var temp = {user : user, notif : 'Incorrect password. Please try again.'};
-                        res.render('profile-edit', temp);
-                    }
+                            // creates updated user object
+                            var update = user;
+                            if (req.body.username != '') update.username = req.body.username;
+                            if (req.body.contact_no != '') update.phone = req.body.contact_no;
+                            if (req.body.bday != '') update.bday = req.body.bday;
+                            if (req.body.degree != '') update.degree = req.body.degree;
+                            if (req.body.bio != '') update.bio = req.body.bio;
+                            if (req.body.n_password != '') update.password = hash;
+
+                            console.log(req.body.password)
+                            console.log(user.password)
+
+
+                            // updates user in db
+                            db.updateOne(User, {username: user.username}, update, function(flag) {
+                                if(flag) res.redirect('/profile?editsuccess=true');
+                                else res.redirect('/profile?editsuccess=false');
+                            });
+                        }
+                        else {
+                            var temp = {user : user, notif : 'Incorrect password. Please try again.'};
+                            res.render('profile-edit', temp);
+                        }
+                    });
                 });
             });
         }  
@@ -112,15 +120,30 @@ const profileController = {
 
             // gets user from db
             db.findOne(User, {username: req.session.username}, '', function (user) {
-
-                // checks if both passwords match user's password
-                if(user.password == req.body.password && user.password == req.body.c_password) {
-
-                    // deletes user from db
-                    db.deleteOne(User, {username: user.username}, function(flag) {
-                        if(flag) res.redirect('/profile-deletion-success');
-                        else res.redirect('/profile?deletesuccess=false');
+                if (req.body.c_password == req.body.password) {
+                    // checks if passwords match user's password 
+                    bcrypt.compare(req.body.password, user.password, function(err, equal) {
+                        if(equal) {
+                            // deletes user from db
+                            db.deleteOne(User, {username: user.username}, function(flag) {
+                                if(flag) {
+                                    req.session.destroy(function(err) {
+                                        if(err) throw err;
+                                        res.redirect('/profile-deletion-success');
+                                    });
+                                }
+                                else res.redirect('/profile?deletesuccess=false');
+                            });
+                        }
+                        else {
+                            var temp = {user : user, notif : 'Incorrect password. Please try again.'};
+                            res.render('profile-del', temp);
+                        }
                     });
+                }
+                else {
+                    var temp = {user : user, notif : 'Incorrect password. Please try again.'};
+                    res.render('profile-del', temp);
                 }
             });
         } 
