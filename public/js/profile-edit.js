b53f02afac750121f8e145c682c4b353c7e73ab5
sophia-vista <75743382+sophia-vista@users.noreply.github.com>
2021-05-21 14:39:24 +0800

    Update profile-edit.js

--- public/js/profile-edit.js
+++ public/js/profile-edit.js
@@ -1,43 +1,14 @@
 $(document).ready(function () {
-    var taskdays = [];
-    var today = (new Date()).getDate();
-    $.get('/getReqsDates', function (result) {
-        if(result) {
-            for(var j in result) {
-                var d = new Date(result[j].deadline);
-                taskdays.push(d.getDate());
-            }
-        }
-        $('.cal-table tr').each(function() {
-            for (let j = 0; j < 7; j++) {
-                if($(this.cells[j]).text() == 'Sunday') j=7;
-                if (parseInt($(this.cells[j]).text()) == today) 
-                    $(this.cells[j]).addClass('today');
+    function setInvalid(field, errormsg, errorfield) {
+        field.css('background-color', '#FFB0B0');
+        errorfield.text(errormsg);
+        $('#signup_button').attr("disabled", true);
+    }
 
-                if($(this.cells[j]).text() == '0') {
-                    $(this.cells[j]).text('');
-                    $(this.cells[j]).addClass('not-month');
-                }
-
-                else if (taskdays.includes(parseInt($(this.cells[j]).text()))){
-                    $(this.cells[j]).addClass('with-task');
-                }
-                else {
-                    $(this.cells[j]).addClass('in-month');
-                }
-            }
-        });
-        $(".with-task").hover(
-            function() {
-                $("a").find('.' + $(this).children('p').text()).css("background-color", "#457b9d");
-                $("a").find('.' + $(this).children('p').text()).children().css("color", "white");
-                $(this).css("background-color", "#7FACC7");
-            }, 
-            function() {
-                $("a").find('.' + $(this).children('p').text()).css("background-color", "#a8dadc");
-                $("a").find('.' + $(this).children('p').text()).children().css("color", "#333333");
-                $(this).css("background-color", "#ECF7F8");
-            }
-        );
-    });
+    function setValid(field, errorfield) {
+        field.css('background-color', '#FFFFFF');
+        errorfield.text('');
+        $('#signup_button').attr("disabled", false);
+    }
+    
 });
