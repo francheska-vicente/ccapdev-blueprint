b55e206ee1974ed7156d05e6b1b1edbc5f0e4672
sophia-vista <75743382+sophia-vista@users.noreply.github.com>
2021-05-21 15:06:34 +0800

    Update profile-edit.js

--- public/js/profile-edit.js
+++ public/js/profile-edit.js
@@ -10,5 +10,23 @@ $(document).ready(function () {
         errorfield.text('');
         $('#signup_button').attr("disabled", false);
     }
-    
+
+    function checkIfValidUsername(username, errorfield) {
+        if (!validator.isAlphanumeric(username.replaceAll("-", '').replaceAll("_", '').replaceAll(".", '')))
+            setInvalid($('#username'), 'Invalid username. Use Alphanumeric characters (A-Z, 0-9), dash (-), period, and underscore (_) only.', errorfield);
+        else if (!validator.isLength(username, {min: 12, max: 20})) 
+            setInvalid($('#username'), 'Invalid username. Minimum of 12 characters and maximum of 20 characters.', errorfield);
+        else $.get('/getCheckUsername', {username: username}, function (result) {
+            if(result.username == username) 
+                setInvalid($('#username'), 'Username is taken!', errorfield);
+            else setValid($('#username'), errorfield);
+        });
+    }
+
+    function checkIfValidContactNo(username, errorfield) {
+        if (!validator.isMobilePhone(str, 'en-PH'))
+            setInvalid($('#username'), 'Invalid PH phone number.', errorfield);
+
+        else setValid($('#username'), errorfield);
+    }
 });
