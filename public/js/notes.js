92eb91b878a0e0bd80491b2f6f015fc41042f46f
francheska-vicente <francheska_vicente@dlsu.edu.ph>
2021-05-22 23:47:11 +0800

    Update notes.js

--- public/js/notes.js
+++ public/js/notes.js
@@ -282,7 +282,7 @@ $(document).ready (function ()
     var URL = window.location.href;
     var route = URL;
     URL = URL.substring (33, URL.length) + '/edit';
-    
+    console.log (URL + " " + route);
 
     $.post (URL, {main_notes_text : $("#main_notes_text").val ()}, function (result) {
         $("#note").load (route + " #note");
